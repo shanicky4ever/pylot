1db00166609de14f325e7dffd4e5af5913862a54
shanicky4ever <shanicky4ever@gmail.com>
2024-04-19 10:56:41 +0000

    minor fix

--- pylot/loggers/obstacle_with_location_logger_operator.py
+++ pylot/loggers/obstacle_with_location_logger_operator.py
@@ -38,5 +38,5 @@ class ObstacleLocationLoggerOperator(erdos.Operator):
             'z': obs.transform.location.z,
             'id': obs.id,
             'label': obs.label,
-            'confidence': obs.confidence,
+            'confidence': float(obs.confidence.numpy()),
         } for obs in obstacles_with_location]
